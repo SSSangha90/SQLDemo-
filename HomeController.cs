40f7cf862b518936628be99d1ef69442b1d70fa4
Sukh Sangha <37102418+SSSangha90@users.noreply.github.com>
2018-07-19 16:04:58 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sqlDBCheck.Models; // to tell VS we're using name of file and the models within it
+
+namespace sqlDBCheck.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CustomersOrdersJulyEntities db = new CustomersOrdersJulyEntities(); // line of code to add to link up the database - name of database file plus entities.
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
